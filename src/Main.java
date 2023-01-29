044395eab572f46a5e007cc2b389a5afdeb994e4
Nitesh Sharma <niteshsharma9199@gmail.com>
2023-01-29 20:41:52 +0530

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> students = new ArrayList<Integer>();
+        students.add(0);
+        students.add(1);
+        students.add(2);
+        students.add(3);
+        students.add(4);
+        students.add(5);
+        System.out.println(students);
+    }
+}
