53bb5903fe27e965e4fd1772e24d219509b67e2e
Nitesh Sharma <niteshsharma9199@gmail.com>
2023-01-30 09:57:59 +0530

    ArrayList methods

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
         ArrayList<Integer> students = new ArrayList<Integer>();
+        //arrayList add method
         students.add(0);
         students.add(1);
         students.add(2);
@@ -10,5 +13,28 @@ public class Main {
         students.add(4);
         students.add(5);
         System.out.println(students);
+        //arrayList Get method
+        System.out.println("value at index 2: "+students.get(2));
+        //arrayList Set method
+        students.set(2,10);
+        System.out.println("value at index 2 after value change: "+ students);
+        // arrayList remove method
+        students.remove(2);
+        System.out.println("value at index 2 after value removed: "+ students);
+        // arrayList all value cleared
+        students.clear();
+        System.out.println("All value cleared: "+ students);
+        //adding values back
+        System.out.println("adding values back...");
+
+        students.add(5);
+        students.add(0);
+        students.add(2);
+        students.add(3);
+        students.add(4);
+        students.add(1);
+        System.out.println(students);
+        Collections.sort(students);
+        System.out.println("sorted arrayList: "+ students);
     }
 }
